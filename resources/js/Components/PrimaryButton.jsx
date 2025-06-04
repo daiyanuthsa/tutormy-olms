79c869290684fb147e25d4e8bf76c5bebcb92e9e
faracutrisfa <faracutrisfa@gmail.com>
2025-06-04 16:49:32 +0700

    refactor primary button

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -5,7 +5,7 @@ export default function PrimaryButton({
     variant = 'fill',
     ...props
 }) {
-    const baseStyles = 'inline-flex items-center text-center justify-center font-bold rounded-full px-6 lg:px-10 py-2 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary-3 focus:ring-offset-2';
+    const baseStyles = 'inline-flex items-center text-center justify-center font-bold rounded-full px-6 lg:px-10 py-3 transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-primary-3 focus:ring-offset-2';
 
     const fillStyles = 'bg-gradient-light hover:bg-gradient-dark text-white focus:bg-gradient-dark active:bg-gradient-dark';
 

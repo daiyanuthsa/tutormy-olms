030287c785c1ea22baccf52990799f03d2bfe875
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-06-07 21:35:40 +0700

    Update CourseDetails.jsx

--- resources/js/Pages/Course/CourseDetails.jsx
+++ resources/js/Pages/Course/CourseDetails.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const CourseDetails = () => {
+const CourseDetails = (course) => {
     return (
-        <div>
-
+        <div className="text-white ">
+            <h1 className="text-white text-2xl font-bold mb-4">
+                Detail Kursus
+            </h1>
+            <pre>{JSON.stringify(course, null, 2)}</pre>
         </div>
-    )
+    );
 }
 
 export default CourseDetails

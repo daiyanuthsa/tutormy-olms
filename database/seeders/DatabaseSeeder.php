ebedb2217dd73daa8dec564a1c521cfcd02133e3
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-06-30 13:23:18 +0700

    Add WebinarSeeder and register it in DatabaseSeeder
    Introduces WebinarSeeder to seed webinar data into the Agenda model and updates DatabaseSeeder to include WebinarSeeder in the seeder call list.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,6 +40,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CourseSeeder::class,
             DocumentSeeder::class,
+            WebinarSeeder::class,
         ]);
 
         

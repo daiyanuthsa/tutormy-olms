8bb27e4e833ff8fdbb3483e6f386f7e91b7ba30c
Daiyan Uthsa Rafif <95560026+daiyanuthsa@users.noreply.github.com>
2025-08-12 03:56:25 +0700

    Delete public/build/assets directory

--- public/build/assets/InputLabel-MFg-_QVP.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DUN3Yeit.js";function l({value:o,className:s="",children:t,...e}){return n.jsx("label",{...e,className:"block font-semibold "+s,children:o||t})}export{l as I};

bb4df3dce350228b898bcf6f571c67166c34ccc4
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2025-08-11 19:19:15 +0700

    Update dependencies in package-lock.json
    Upgraded several Babel and esbuild packages to their latest versions, including @babel/core, @babel/generator, @babel/helpers, @babel/parser, @babel/traverse, @babel/types, and all @esbuild platform-specific packages. Also updated @floating-ui/core, @floating-ui/dom, and @floating-ui/react-dom to newer versions. These updates improve compatibility, performance, and security.

--- public/build/assets/InputLabel-0Ej1KM6c.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-6mCIMYdR.js";function l({value:o,className:s="",children:t,...e}){return n.jsx("label",{...e,className:"block font-semibold "+s,children:o||t})}export{l as I};
